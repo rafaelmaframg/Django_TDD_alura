855eb60cfc674b9f12b10d6f506db2a00d75b0ee
Rafael Mafra <rafaelmafr4@hotmail.com>
2021-10-22 17:50:03 +0100

    deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn tw_clientes.wsgi
+web: gunicorn setup.wsgi
--- setup/settings.py
+++ setup/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
